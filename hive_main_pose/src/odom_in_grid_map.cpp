8299d3c16d2279394deb03908882004e1f1a7458
taehun-ryu <xogns2079@gmail.com>
2024-04-26 22:00:17 +0900

    feat: change message type from std_msgs/multiarray to geometry_msgs/pose_2d.

--- hive_main_pose/src/odom_in_grid_map.cpp
+++ hive_main_pose/src/odom_in_grid_map.cpp
@@ -1,6 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/odometry.hpp"
-#include "std_msgs/msg/u_int8_multi_array.hpp"
+#include "geometry_msgs/msg/pose2_d.hpp"
 
 class GridOdom : public rclcpp::Node
 {
@@ -8,7 +8,7 @@ class GridOdom : public rclcpp::Node
   GridOdom(/* args */);
   ~GridOdom();
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr final_pose_sub_;
-  rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr grid_odom_pub_;
+  rclcpp::Publisher<geometry_msgs::msg::Pose2D>::SharedPtr grid_odom_pub_;
   void finalPoseCallback(const nav_msgs::msg::Odometry::SharedPtr final_pose_msg);
 
  private:
@@ -19,7 +19,7 @@ GridOdom::GridOdom(/* args */) : Node("grid_odom_node")
 {
   final_pose_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
       "robot1/final_pose", 10, std::bind(&GridOdom::finalPoseCallback, this, std::placeholders::_1));
-  grid_odom_pub_ = this->create_publisher<std_msgs::msg::UInt8MultiArray>("robot1/grid_odom", 10);
+  grid_odom_pub_ = this->create_publisher<geometry_msgs::msg::Pose2D>("robot1/grid_odom", 10);
 }
 
 GridOdom::~GridOdom()
@@ -32,17 +32,18 @@ void GridOdom::finalPoseCallback(const nav_msgs::msg::Odometry::SharedPtr final_
   double scaling_width = 0.0074;     // Real: 1px = 0.0074m
   double scaling_height = 0.0058;    // Real: 1px = 0.0058m
 
-  double world_coords_x = 0;
-  double world_coords_y = 0;
+  double world_coords_x;
+  double world_coords_y;
   world_coords_x = final_pose_msg->pose.pose.position.x;
   world_coords_y = final_pose_msg->pose.pose.position.y;
 
   int grid_x = world_coords_x / scaling_width;
   int grid_y = world_coords_y / scaling_height;
 
-  std_msgs::msg::UInt8MultiArray grid_odom;
-  grid_odom.data.push_back(grid_x);
-  grid_odom.data.push_back(grid_y);
+  geometry_msgs::msg::Pose2D grid_odom;
+  grid_odom.x = grid_x;
+  grid_odom.y = grid_y;
+  grid_odom.theta = final_pose_msg->pose.pose.orientation.z;
 
   grid_odom_pub_->publish(grid_odom);
 }
